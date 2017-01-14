71da73e87caf8a9a6772d0ee8fb623886bf8d50f
Stephen Toon <satoon101@gmail.com>
2017-01-13 21:18:30 -0500

    Updated rules and added rules translation file.

--- addons/source-python/plugins/gungame/plugins/custom/gg_assists/rules.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_assists/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,10 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 assists_rules = GunGameRules(info.name)
-assists_rules.title = 'AssistsRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('AssistsRules:'):
-        assists_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
+assists_rules.register_all_rules()
